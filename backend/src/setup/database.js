67993fe8e0901bbf08db5efc22319e0866caad56
robertov92 <vil13004@byui.edu>
2021-07-01 19:18:20 -0500

    adding get-polls, create-votes, and create-votes validation

--- backend/src/setup/database.js
+++ backend/src/setup/database.js
@@ -3,7 +3,9 @@ const Mongodb = require('mongodb');
 const uri = 'mongodb+srv://roberto:Unoyuno5@cluster0.zwdeu.mongodb.net/Project2?retryWrites=true&w=majority';
 
 module.exports = async() => {
-    const client = new Mongodb.MongoClient(uri);
+    const client = new Mongodb.MongoClient(uri, {
+        useUnifiedTopology: true
+    });
     await client.connect();
 
     return client.db();
