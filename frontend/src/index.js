40809db32851728f7bca03a554a152f43274f90f
robertov92 <vil13004@byui.edu>
2021-07-03 11:28:11 -0500

    adding router and home page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './pages/Home';
+
+ReactDOM.render(
+<BrowserRouter>
+  <div className="w-full h-screen">
+    <div className="h-16 w-full bg-gray-800 flex items-center">
+      <div className="container mx-auto px-5">
+        <Link to='/' className='text-white cursor-pointer hover:text-gray-400 transition duration-150 mr-3'>Home</Link>
+        <Link to='/polls/create' className='text-white cursor-pointer hover:text-gray-400 transition duration-150 mr-3'>Create Poll</Link>
+      </div>
+    </div>
+    <Switch>
+      <Route exact path='/' component={Home} />
+    </Switch>
+  </div>
+</BrowserRouter>,
+document.getElementById('root'));
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className='container mx-auto px-5'>
+      <h1 className='text-5xl text-center my-10'>Welcome to the Voting Platform</h1>
+    </div>
+  );
+}
+
+export default Home;
