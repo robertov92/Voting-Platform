1afca09fbf66092e1c9f46b94b2193867a54c527
robertov92 <vil13004@byui.edu>
2021-07-13 14:22:41 -0500

    Updates

---
+++ precache-manifest.6f033dd34bdb473701a48837e254c9a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d239fcfbcb294905f185ef6ec48191",
+    "url": "/Voting-Platform/index.html"
+  },
+  {
+    "revision": "cb3fa1f854c1246f8f50",
+    "url": "/Voting-Platform/static/js/2.c7133ff8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Voting-Platform/static/js/2.c7133ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9ba6c14c5688bde605",
+    "url": "/Voting-Platform/static/js/main.c38bc5c3.chunk.js"
+  },
+  {
+    "revision": "e766ac8bdbf4a7c4ecc2",
+    "url": "/Voting-Platform/static/js/runtime-main.4affcd86.js"
+  }
+]);
